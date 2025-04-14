928f3d362063d085e6e57a1e252235388fff8154
zsj <cho010105@naver.com>
2025-04-14 10:56:58 +0900

    [EDIT] server change

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/api/return', upload.single('image'), async (req, res) => {
       body: JSON.stringify({
         toPersonEmail: email,
         text: '📸 근처에 불법 주차된 PM이 있습니다. 위치를 조정해주세요!',
-        files: [`https://5851-210-119-237-101.ngrok-free.app/uploads/20250409_reAdjustPM.jpg`]
+        files: [`https://noble-tammara-kicksco-97f46231.koyeb.app/uploads/20250409_reAdjustPM.jpg`]
       })
     });
 
